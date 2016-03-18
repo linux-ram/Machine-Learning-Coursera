bb6206b63d4f1f7da955d88fa24da8647fe43da3
Ram Subramanian <rsubramanian@coe.neu.edu>
2016-03-17 20:51:36 -0400

    Added files via upload

---
+++ Multi-class-Classification-and-Neural-Networks/predict.m
@@ -0,0 +1,37 @@
+function p = predict(Theta1, Theta2, X)
+%PREDICT Predict the label of an input given a trained neural network
+%   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
+%   trained weights of a neural network (Theta1, Theta2)
+
+% Useful values
+m = size(X, 1);
+num_labels = size(Theta2, 1);
+
+% You need to return the following variables correctly 
+p = zeros(size(X, 1), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the following code to make predictions using
+%               your learned neural network. You should set p to a 
+%               vector containing labels between 1 to num_labels.
+%
+% Hint: The max function might come in useful. In particular, the max
+%       function can also return the index of the max element, for more
+%       information see 'help max'. If your examples are in rows, then, you
+%       can use max(A, [], 2) to obtain the max for each row.
+%
+X=[ones(size(X,1),1) X];
+activation_hidden_layer=sigmoid(Theta1*X')';
+activation_hidden_layer=[ones(size(activation_hidden_layer,1),1) activation_hidden_layer];
+p_soft=sigmoid(Theta2*activation_hidden_layer')';
+[~,p]=max(p_soft, [], 2);
+
+
+
+
+
+
+% =========================================================================
+
+
+end
---
+++ README.md
@@ -0,0 +1,3 @@
+## Programming Exercises from the Coursera MOOC 'Machine Learning'
+
+Refer to the PDF in each folder. There are 8 Programming Exercises in total.
